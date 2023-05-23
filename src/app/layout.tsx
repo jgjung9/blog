82478f27da8ef4b3e218d2eec110706a9c2f4acf
gio <wnsgyo9@gmail.com>
2023-05-23 17:02:49 +0900

    Add main tag in homelayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className + 'w-full'}>
         <Header />
-        {children}
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
